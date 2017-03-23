1be304ba2cd5aaaf49c7108f6c7e1fdc0d65d24e
Nikolay Bogoychev <nheart@gmail.com>
2017-03-23 00:03:53 +0000

    Exp now happens in gLM

--- nematus/nmt.py
+++ nematus/nmt.py
@@ -353,7 +353,7 @@ def build_model(tparams, options):
     #print(probs.tag.test_value.shape)
     #@TODO. ngram_scores are log probs. Also what sort of interpolation do i use?
     if 'ngram_weight' in tparams:
-        probs = (1-tparams['ngram_weight'])*probs + tensor.exp(ngram_scores)*tparams['ngram_weight']
+        probs = (1-tparams['ngram_weight'])*probs + ngram_scores*tparams['ngram_weight']
     #print("Debug dimensions2:")
     #print(probs.tag.test_value.shape)
 
